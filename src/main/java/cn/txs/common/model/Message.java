ef1a57fe55ba5549bbf169f0d9b683aa91cf6d24
txs <213@123.com>
2018-05-22 18:28:02 +0800

    init

---
+++ src/main/java/cn/txs/common/model/Message.java
@@ -0,0 +1,9 @@
+package cn.txs.common.model;
+
+/**
+ * @author tangxiaoshuang
+ * @date 2018/5/22 18:25
+ */
+public class Message {
+    private String type;
+}
---
+++ src/main/java/cn/txs/websocket/ChatContext.java
@@ -0,0 +1,9 @@
+package cn.txs.websocket;
+
+/**
+ * @author tangxiaoshuang
+ * @date 2018/5/22 18:23
+ */
+public class ChatContext {
+
+}
