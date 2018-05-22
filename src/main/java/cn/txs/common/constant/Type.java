0e0fe3c1a0711edcc477a53c50bb7d4e397e347e
txs <213@123.com>
2018-05-22 18:29:50 +0800

    init

---
+++ src/main/java/cn/txs/common/constant/Type.java
@@ -0,0 +1,8 @@
+package cn.txs.common.constant;
+
+/**
+ * @author tangxiaoshuang
+ * @date 2018/5/22 18:29
+ */
+public enum Type {
+}
--- src/main/java/cn/txs/common/model/Message.java
+++ src/main/java/cn/txs/common/model/Message.java
@@ -1,9 +1,11 @@
 package cn.txs.common.model;
 
+import cn.txs.common.constant.Type;
+
 /**
  * @author tangxiaoshuang
  * @date 2018/5/22 18:25
  */
 public class Message {
-    private String type;
+    private Type type;
 }
